fdb087f3265c43b46fabae232652ec797f9d93a1
khamidullox <hamidullo22022@gmail.com>
2024-02-15 12:14:32 +0500

    js Type Conversion

---
+++ app.js
@@ -0,0 +1,18 @@
+// let a = "205" ;
+// console.log(Number(a)+1)
+//  let a = 'Hamidullo';
+//  console.log(Number(a)+1)
+
+// console.log('Men ' + 2006 + 'chi yilda tugilganman'); 
+// console.log('30' - '50' - 10);
+
+// console.log(Boolean(false))
+// console.log(Boolean(0))
+// console.log(Boolean(''))
+// console.log(Boolean(undefined))
+// console.log(Boolean(null))
+// console.log(Boolean(NaN))
+// console.log(Boolean(0n))
+
+
+
